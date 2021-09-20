abf7cba568f148f337f788c3b5390ef38acb0219
Peter Mourfield <1678724+peter-mourfield@users.noreply.github.com>
2021-09-20 16:27:05 -0400

    add dockerfile (work in progress)

---
+++ setup.sql
@@ -0,0 +1,8 @@
+IF DB_ID('StackOverflow') IS NULL
+    CREATE DATABASE StackOverflow   
+        ON (FILENAME = '/stack-overflow/StackOverflow2013_1.mdf'),   
+        (FILENAME = '/stack-overflow/StackOverflow2013_2.ndf'),   
+        (FILENAME = '/stack-overflow/StackOverflow2013_3.ndf'),   
+        (FILENAME = '/stack-overflow/StackOverflow2013_4.ndf'),   
+        (FILENAME = '/stack-overflow/StackOverflow2013_log.ldf')   
+        FOR ATTACH;  
